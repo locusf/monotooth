e408e8be1d403e371c918fa183ccc9acf6956b4e
Aleksi Suomalainen <suomalainen.aleksi@gmail.com>
2008-04-21 16:46:31 +0300

    tests

---
+++ tests/GenericTests.cs
@@ -0,0 +1,39 @@
+/*
+ * Created by SharpDevelop.
+ * User: LoCusF
+ * Date: 20.4.2008
+ * Time: 22:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using NUnit.Framework;
+using System;
+
+
+namespace tests
+{
+	[TestFixture]
+	public class GenericTests
+	{
+		[Test]
+		[ExpectedException(typeof(Exception))]
+		public void TestNonPlugged()
+		{
+			monotooth.Device.ILocalDevice dev = monotooth.Device.LocalDevice.Default;
+			dev.Inquire();
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			// TODO: Add Init code.
+		}
+		
+		[TestFixtureTearDown]
+		public void Dispose()
+		{
+			// TODO: Add tear down code.
+		}
+	}
+}
