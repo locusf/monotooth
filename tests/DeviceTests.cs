81a0502753844b5395c3c8ea074f70402ec347f7
Aleksi Suomalainen <suomalainen.aleksi@gmail.com>
2008-05-27 10:04:34 +0300

    Restructured tests.

---
+++ tests/DeviceTests.cs
@@ -0,0 +1,36 @@
+/*
+ * Created by SharpDevelop.
+ * User: LoCusF
+ * Date: 27.5.2008
+ * Time: 10:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using NUnit.Framework;
+using System;
+
+namespace tests
+{
+	[TestFixture]
+	public class DeviceTests
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			// TODO: Add Init code.
+		}
+		
+		[TestFixtureTearDown]
+		public void Dispose()
+		{
+			// TODO: Add tear down code.
+		}
+	}
+}
